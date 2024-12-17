2b6dd1f30a5081d23d91e7799a29095a90382255
Ghulam Nabi <syed.g.nabi@proton.me>
2024-12-17 23:16:03 +0500

    added LAB14

--- LABTasks/LAB 14/Graph.java
+++ LABTasks/LAB 14/Graph.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.PriorityQueue;
 
 public class Graph{
 
@@ -37,7 +38,77 @@ public class Graph{
         Pair pair2 = new Pair(source, weight);
         graph[destination].add(pair2);
     }
-    
+    public void getAllNeighbours(){
+        for(int i = 0; i < numOfVertices; i++){
+            System.out.print("Node: "+i+" => ");
+            for(int j = 0; j < graph[i].size(); j++){
+                System.out.print("["+graph[i].get(j).node+" "+graph[i].get(j).weight+"] ");
+            }
+            System.out.println();
+        }
+    }
+   
+    public int[] dijkstra(int startNode) {
+        int[] distances = new int[numOfVertices];
+        boolean[] visited = new boolean[numOfVertices];
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
 
+        for (int i = 0; i < numOfVertices; i++) {
+            distances[i] = Integer.MAX_VALUE;
+            visited[i] = false;
+        }
+        distances[startNode] = 0;
+        pq.add(new Pair(startNode, 0));
+
+        while (!pq.isEmpty()) {
+            Pair current = pq.poll();
+            int currentNode = current.node;
+
+            if (visited[currentNode]) continue;
+            visited[currentNode] = true;
+
+            for (Pair neighbor : graph[currentNode]) {
+                int newDist = distances[currentNode] + neighbor.weight;
+                if (newDist < distances[neighbor.node]) {
+                    distances[neighbor.node] = newDist;
+                    pq.add(new Pair(neighbor.node, newDist));
+                }
+            }
+        }
+        return distances;
+    }
+        public int[] primsAlgorithm(int startNode) {
+            int[] parent = new int[numOfVertices];
+            int[] key = new int[numOfVertices];
+            boolean[] inMST = new boolean[numOfVertices];
+            PriorityQueue<Pair> pq = new PriorityQueue<>();
+
+            for (int i = 0; i < numOfVertices; i++) {
+                key[i] = Integer.MAX_VALUE;
+                inMST[i] = false;
+                parent[i] = -1;
+            }
+            key[startNode] = 0;
+            pq.add(new Pair(startNode, 0));
+
+            while (!pq.isEmpty()) {
+                Pair current = pq.poll();
+                int currentNode = current.node;
+
+                if (inMST[currentNode]) continue;
+                inMST[currentNode] = true;
+
+                for (Pair neighbor : graph[currentNode]) {
+                    if (!inMST[neighbor.node] && neighbor.weight < key[neighbor.node]) {
+                        key[neighbor.node] = neighbor.weight;
+                        pq.add(new Pair(neighbor.node, key[neighbor.node]));
+                        parent[neighbor.node] = currentNode;
+                    }
+                }
+            }
+            return parent;
+        }
+    
     
 }
+
--- LABTasks/LAB 14/Main.java
+++ LABTasks/LAB 14/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello wORd!");
-        
+        Graph graph = new Graph(5);
+        graph.addUndirectedEdge(0, 1, 3);
+        graph.addUndirectedEdge(0, 2, 2);
+        graph.addUndirectedEdge(1, 3, 4);
+        graph.addUndirectedEdge(2, 4, 1);
+        graph.addUndirectedEdge(3, 4, 5);
+        graph.addUndirectedEdge(0, 4, 6);
+        graph.addUndirectedEdge(1, 2, 7);
+
+        graph.getAllNeighbours();
     }
 }

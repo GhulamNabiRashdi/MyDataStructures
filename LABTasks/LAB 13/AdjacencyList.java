b8243d6578a573f58c19cb22477b839cfea89f9d
Ghulam Nabi <syed.g.nabi@proton.me>
2024-12-17 13:54:58 +0500

    Added lab 13

--- LABTasks/LAB 13/AdjacencyList.java
+++ LABTasks/LAB 13/AdjacencyList.java
@@ -15,18 +15,31 @@ public class AdjacencyList {
     private int size;
 
     public AdjacencyList(int size) {
+        this.size = size;
         adjacencyList = new ArrayList<>(size);
         initializer(adjacencyList);
     }
 
     private void initializer(ArrayList<ArrayList<Edge>> adjacencyList) {
-        for (int i = 0; i < adjacencyList.size(); i++) {
+        for (int i = 0; i <size; i++) {
             adjacencyList.add(new ArrayList<>());
         }
 
     }
 
-    public void addEdge(int source, AdjacencyList.Edge destination) {
-        adjacencyList.get(source).add(destination);
+    public void addEdge(int source, int destination) {
+        Edge edge = new Edge(source, destination);
+        adjacencyList.get(source).add(edge);
+        adjacencyList.get(destination).add(edge);
+    }
+    public void printGraph(){
+        for(int i = 0; i < size; i ++){
+            System.out.print("Neighbours of " + i + " are: ");
+            for(int j = 0; j < adjacencyList.get(i).size(); j++){
+                System.out.print(adjacencyList.get(i).get(j).destination + " ");
+            
+            }
+            System.out.println();
+        }
     }
 }
